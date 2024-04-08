326ccf58a7a2b6812013219219499b66bc3bbde6
Dan Wahlin <dwahlin@xmlforasp.net>
2024-04-07 23:22:58 -0700

    Update form & add sln

--- Module 2 - The Role of Events Delegates and Event Handlers/BasicWindowsEvents/Form1.Designer.cs
+++ Module 2 - The Role of Events Delegates and Event Handlers/BasicWindowsEvents/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,18 +23,69 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.CountriesComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(27, 100);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 0;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // CountriesComboBox
+            // 
+            this.CountriesComboBox.FormattingEnabled = true;
+            this.CountriesComboBox.Items.AddRange(new object[] {
+            "USA",
+            "Mexico",
+            "Canada"});
+            this.CountriesComboBox.Location = new System.Drawing.Point(27, 45);
+            this.CountriesComboBox.Name = "CountriesComboBox";
+            this.CountriesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.CountriesComboBox.TabIndex = 1;
+            this.CountriesComboBox.SelectedIndexChanged += new System.EventHandler(this.CountriesComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Countries";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CountriesComboBox);
+            this.Controls.Add(this.SubmitButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.ComboBox CountriesComboBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Module 2 - The Role of Events Delegates and Event Handlers/BasicWindowsEvents/Form1.cs
+++ Module 2 - The Role of Events Delegates and Event Handlers/BasicWindowsEvents/Form1.cs
@@ -17,5 +17,20 @@ namespace BasicWindowsEvents
             InitializeComponent();
         }
 
+        private void CountriesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Current index " + CountriesComboBox.SelectedIndex.ToString());
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked me");
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(e.X.ToString());
+        }
+
     }
 }
